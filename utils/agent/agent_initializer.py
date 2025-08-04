79b36323b35ddc5e7b2a1d7d51eb47becf560bce
shreysheth <shreysheth6487@gmail.com>
2025-08-04 22:25:08 +0530

    update agent initializer

--- utils/agent/agent_initializer.py
+++ utils/agent/agent_initializer.py
@@ -7,17 +7,10 @@ from langchain_core.prompts import PromptTemplate
 from prompts import AGENT_PROMPT
 
 logger.info("Initializing agent with vector_db_context_search_tool and mongo_db_sql_tool")
-# agent = initialize_agent(
-#     tools=[vector_db_context_search_tool, mongo_db_sql_tool],
-#     llm=llm,
-#     agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-#     verbose=True
-# )
 
 prompt = PromptTemplate.from_template(AGENT_PROMPT)
 tools = [vector_db_context_search_tool, mongo_db_sql_tool]
-# agent = create_react_agent(llm, tools, prompt)
-# llm_agent = llm.bind_tools(tools)
+
 agent = create_react_agent(llm, tools, prompt)
 
 logger.info("Agent initialized successfully")
